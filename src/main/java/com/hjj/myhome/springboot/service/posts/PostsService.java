296eb1581d450878af7be6b57dc2d09f83967292
hjj1991 <hjj19911@naver.com>
2020-05-02 18:54:45 +0900

    메인화면작성

--- src/main/java/com/hjj/myhome/springboot/service/posts/PostsService.java
+++ src/main/java/com/hjj/myhome/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.hjj.myhome.springboot.service.posts;
 
 import com.hjj.myhome.springboot.domain.posts.Posts;
 import com.hjj.myhome.springboot.domain.posts.PostsRepository;
+import com.hjj.myhome.springboot.web.dto.PostsListResponseDto;
 import com.hjj.myhome.springboot.web.dto.PostsResponseDto;
 import com.hjj.myhome.springboot.web.dto.PostsSaveRequestDto;
 import com.hjj.myhome.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,10 +10,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
